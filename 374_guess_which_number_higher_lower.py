18d96d93178304723c1fdc3973a558a18a81db98
Alex <114017003+synalex@users.noreply.github.com>
2022-11-16 22:13:13 +0100

    Create 374_guess_which_number_higher_lower.py

---
+++ 374_guess_which_number_higher_lower.py
@@ -0,0 +1,28 @@
+# The guess API is already defined for you.
+# @param num, your guess
+# @return -1 if num is higher than the picked number
+#          1 if num is lower than the picked number
+#          otherwise return 0
+# def guess(num: int) -> int:
+
+class Solution:
+    def guessNumber(self, n: int) -> int:
+        """
+        Time complexity : O(log2(n))
+        Space complexity : O(1)
+        """
+        left = 1
+        right = n       
+        while right >= left:
+            mid = (left + right) // 2
+            i = guess(mid)
+            print(mid)
+            if i == 1:             
+                left = mid + 1     
+            elif i == -1:
+                right = mid - 1
+            elif i == 0:
+                print("k")
+                return mid
+            else:
+                print("bug")
